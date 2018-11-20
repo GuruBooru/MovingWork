487f918f8d9ef1fd779dcfa793b04a12d58b112d
GuruBooru <123gudehsdl@naver.com>
2018-11-21 00:52:12 +0900

    프로젝트 올리기17

---
+++ Assets/05.Openpose/Editor/HumanSetting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace OpenPose.Example {
+	[CustomEditor(typeof(HumanController2D))]
+	public class HumanSetting : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+			
+			HumanController2D myScript = (HumanController2D)target;
+			//if(GUILayout.Button("Run"))
+			//{
+				//myScript.Run();
+			//}
+		}
+	}
+}
